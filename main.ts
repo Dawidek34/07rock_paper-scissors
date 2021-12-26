99a4766dd0b41daddcde40c0b9aa3c3330b50777
Dawidek34 <96680609+Dawidek34@users.noreply.github.com>
2021-12-26 14:40:43 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,66 @@
+function image () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.clearScreen()
+}
+let x = 0
+basic.forever(function () {
+    if (TobbieII.LBlock(900) || TobbieII.RBlock(900)) {
+        image()
+        x = 1 + randint(0, 2)
+        TobbieII.rightward()
+        basic.pause(2500)
+        TobbieII.stopturn()
+        if (x == 1) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                # # . # #
+                # # . # #
+                `)
+        }
+        if (x == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        }
+        if (x == 3) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        }
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+        basic.pause(2000)
+    }
+})
